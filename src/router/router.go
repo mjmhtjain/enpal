87ba8b8325a6a2eee43b3c5acc42f3c906c6e2b7
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2025-03-04 20:08:41 +0100

    missing health route

--- src/router/router.go
+++ src/router/router.go
@@ -11,6 +11,9 @@ func Setup() *gin.Engine {
 	router := gin.Default()
 
 	// Register routes
+	router.GET("/health", handlers.NewHealthHandler().Check)
+
+	// Register routes
 	router.POST("/calendar/query", handlers.NewAppointmentHandler().Find)
 
 	return router
